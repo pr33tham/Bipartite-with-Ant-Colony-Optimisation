4dd535b8040707da7fc7cca649d62bb9b3f94b80
Ruthwik Preetham <ruthwikpr33tham@gmail.com>
2025-02-19 19:55:25 -0500

    update cityGenerator Location

--- Assets/City/CityGenerator.cs
+++ Assets/City/CityGenerator.cs
@@ -2,12 +2,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CityGenerator : MonoBehaviour {
-    [SerializeField] private GameObject parent; 
-    [SerializeField] private Material cityMaterial;
+    [SerializeField] private GameObject parent;
+    [SerializeField] private Material material;
     [SerializeField] private int numberOfCities = 1;
-    private float xBound = 8.5f; 
-    private float yBound = 4.5f; 
-    private List<Vector3> cityPositions = new List<Vector3>();  
+    private float xBound = 8.5f;
+    private float yBound = 4.5f;
+    public List<Vector3> cityPositions = new List<Vector3>();  // [ (-1, -2, 0), (-4, 6, 0), (8.5, 4.5, 0), (-3, 7, 0)]
 
     void Start() {
         for (int i = 0; i < numberOfCities; i++) {
@@ -18,17 +18,19 @@ public class CityGenerator : MonoBehaviour {
     }
 
     private Vector3 GetRandomCityPosition() {
-        float xPos = Random.Range(-xBound, xBound); 
-        float yPos = Random.Range(-yBound, yBound); 
+        float xPos = Random.Range(-xBound, xBound); // -8.5 to 8.5 4 times
+        float yPos = Random.Range(-yBound, yBound); //-4.5 to 4.5
         return new Vector3(xPos, yPos, 0);
     }
 
     private void CreateCity(Vector3 position) {
         GameObject city = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-
         city.transform.position = position;
         city.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
-        city.GetComponent<Renderer>().material = cityMaterial;
+        city.GetComponent<Renderer>().material = material;
         city.transform.SetParent(parent.transform);
     }
+
+
+
 }
