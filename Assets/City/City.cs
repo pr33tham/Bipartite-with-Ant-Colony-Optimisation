8e5c127c90357e60002819e055cf8991d83da726
Ruthwik Preetham <ruthwikpr33tham@gmail.com>
2025-03-09 01:16:07 -0500

    Update City.cs

--- Assets/City/City.cs
+++ Assets/City/City.cs
@@ -1,28 +1,31 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Linq;
 using UnityEngine;
 public class City {
 
-    public Vector3 position;
+    public string name;
     public string type;
+    public Vector3 position;
 
-    public City(string _type, Vector3 location) {
-        position = location;
+    public City(string _name, string _type, Vector3 location) {
+        name = _name;
         type = _type;
+        position = location;
     }
 
     // Override Equals method
     public override bool Equals(object obj) {
         if (obj is City other) {
-            return type == other.type && position == other.position;
+            return type == other.type && name == other.name;
         }
         return false;
     }
 
     // Override GetHashCode for dictionary/hashset compatibility
     public override int GetHashCode() {
-        return type.GetHashCode() ^ position.GetHashCode();
+        return name.GetHashCode() ^ type.GetHashCode();
     }
 
     // Overloading == operator
@@ -35,5 +38,9 @@ public class City {
     // Overloading != operator
     public static bool operator !=(City a, City b) {
         return !(a == b);
+    }
+
+    public override string ToString() {
+        return $"City: {name}";
     }
 }
