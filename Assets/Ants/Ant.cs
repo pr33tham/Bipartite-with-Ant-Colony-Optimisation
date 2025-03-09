86dd4c6096a91baca550195d92c5436d5b93a4d4
Ruthwik Preetham <ruthwikpr33tham@gmail.com>
2025-03-09 03:50:36 -0400

    intial ant commit

--- Assets/Ants/Ant.cs
+++ Assets/Ants/Ant.cs
@@ -3,20 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Ant : MonoBehaviour {
-    //public enum State {
-    //    SearchingFood,
-    //}
+    private void Start() {
 
-
-    //private void Start() {
-
-    //}
-
-    //private float Desirability() {
-
-    //}
-
-    //private void DepositPheramone() {
-
-    //}
+    }
+    private void Update() {
+        
+    }
 }
