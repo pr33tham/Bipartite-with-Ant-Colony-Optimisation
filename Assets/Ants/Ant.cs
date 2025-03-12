8d3c3fa228bc3d8bacaa334c0da58cb77d4942d9
Ruthwik Preetham <ruthwikpr33tham@gmail.com>
2025-03-12 01:29:34 -0400

    graphic changes

--- Assets/Ants/Ant.cs
+++ Assets/Ants/Ant.cs
@@ -3,9 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Ant : MonoBehaviour {
+
+    //Only an Agent that moves around cities instead of biological ants
+    //Can implement later
+    public enum State { searchingSetA, searchingSetB };
+    public Transform pheramoneDepositPos;
+
+
+
+    List<City> VisitedCities;
+
     private void Start() {
 
     }
+
     private void Update() {
         
     }
--- Assets/Ants/AntParams.cs
+++ Assets/Ants/AntParams.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu()]
 public class AntParams : ScriptableObject {
 
     [Header("Movement")]
-    public float antSpeed;
+    public float antSpeed = 1f;
 
     [Header("Pheromones")]
     public float dstBetweenMarkers = 0.75f;
@@ -13,13 +14,12 @@ public class AntParams : ScriptableObject {
     public float pheromoneRunOutTime = 30;
     public float pheromoneWeight = 1;
     public float perceptionRadius = 2.5f;
-    public bool useHomeMarkers = true;
-    public bool useFoodMarkers = true;
+    public bool useFirstMarkers = true;
+    public bool useSecondMarkers = true;
 
     [Header("Sensing")]
     public float sensorSize = 0.75f;
     public float sensorDst = 1.25f;
     public float sensorSpacing = 1;
     public float antennaDst = 0.25f;
-
 }
